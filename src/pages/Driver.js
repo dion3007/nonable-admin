c58230e1e5ce289253d97b0b1c2c18a99b5a9d66
dev4 <aulia.yusdion@sinbad.co.id>
2022-03-09 15:13:34 +0700

    Update Driver.js

--- src/pages/Driver.js
+++ src/pages/Driver.js
@@ -226,7 +226,7 @@ export default function Driver() {
                           <TableCell align="left">{licenseNumber}</TableCell>
                           <TableCell align="left">
                             {employeeType === 3 && 'Casual'}
-                            {employeeType === 2 && 'Terminate'}
+                            {employeeType === 2 && 'Terminated'}
                             {employeeType === 1 && 'Permanent'}
                             {employeeType === 0 && 'Part Time'}
                           </TableCell>
